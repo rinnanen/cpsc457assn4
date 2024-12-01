5b411894992327aaa770c647297108e2110fe5ee
rinnanen <115198032+rinnanen@users.noreply.github.com>
2024-12-01 14:06:39 -0700

    sol3 main func

--- solutionThree.c
+++ solutionThree.c
@@ -12,14 +12,18 @@ struct Semaphore mutex_sem;
 
 
 void *reader3(void *arg) {
-    int thread_id = *((int *)arg); 
+    int thread_id = *((int *)arg);
+    printf("Reader %d is attempting to read.\n", thread_id);
+
     new_wait(&mutex_sem);
     if (writers_count > 0 || readers_count == 0) {
+        printf("Reader %d is waiting as a writer is active or no readers are present.\n", thread_id);
         new_signal(&mutex_sem);
         new_wait(&resource_sem3);
         new_wait(&mutex_sem);
     }
     readers_count++;
+    printf("Reader %d is reading. Total readers: %d.\n", thread_id, readers_count);
     new_signal(&mutex_sem);
 
     // read the resource
@@ -27,7 +31,9 @@ void *reader3(void *arg) {
 
     new_wait(&mutex_sem);
     readers_count--;
+    printf("Reader %d has finished reading. Remaining readers: %d.\n", thread_id, readers_count);
     if (readers_count == 0) {
+        printf("Reader %d is releasing the resource as no readers are left.\n", thread_id);
         new_signal(&resource_sem3);
     }
     new_signal(&mutex_sem);
@@ -37,58 +43,63 @@ void *reader3(void *arg) {
 //errors if name is only writer
 void *writer3(void *arg) {
     int thread_id = *((int *)arg); //should cause errors relating to pthread_create to stop
+    printf("Writer %d is attempting to write.\n", thread_id);
+
     new_wait(&mutex_sem);
     writers_count++;
+    printf("Writer %d is waiting to acquire the resource. Total writers: %d.\n", thread_id, writers_count);
     new_signal(&mutex_sem);
     new_wait(&resource_sem3);
 
     // read the resource
+    printf("Writer %d is writing.\n", thread_id);
     sleep(1);
 
     new_wait(&mutex_sem);
     writers_count--;
+    printf("Writer %d has finished writing. Remaining writers: %d.\n", thread_id, writers_count);
     new_signal(&mutex_sem);
     new_signal(&resource_sem3);
     return NULL;
 }
 
 // duplicated it from sol1
-// int main(){
-//     int randomVal = (rand() % (10 - 0 + 1)) + 0;
-//     pthread_t reader_threads[10], writer_threads[randomVal];
-//     int reader_ids[10], writer_ids[randomVal];
+int main(){
+    int randomVal = (rand() % (10 - 0 + 1)) + 0;
+    pthread_t reader_threads[10], writer_threads[randomVal];
+    int reader_ids[10], writer_ids[randomVal];
 
-//     //initialize semaphores
-//     make_sem(&resource_sem3, 1);
-//     make_sem(&mutex_sem, 1);
+    //initialize semaphores
+    make_sem(&resource_sem3, 1);
+    make_sem(&mutex_sem, 1);
 
-//     //create 10 reader threads
-//     for (int i = 0; i < 10; i++) {
-//         reader_ids[i] = i + 1;  //assign unique id to readers
-//         pthread_create(&reader_threads[i], NULL, reader3, &reader_ids[i]);
-//     }
+    //create 10 reader threads
+    for (int i = 0; i < 10; i++) {
+        reader_ids[i] = i + 1;  //assign unique id to readers
+        pthread_create(&reader_threads[i], NULL, reader3, &reader_ids[i]);
+    }
 
-//     // create n writer threads
-//     for (int i = 0; i < randomVal; i++) {
-//         writer_ids[i] = i + 1;  //assign unique id to writer
-//         pthread_create(&writer_threads[i], NULL, writer3, &writer_ids[i]);
-//     }
+    // create n writer threads
+    for (int i = 0; i < randomVal; i++) {
+        writer_ids[i] = i + 1;  //assign unique id to writer
+        pthread_create(&writer_threads[i], NULL, writer3, &writer_ids[i]);
+    }
 
-//     //wait for all reader threads to complete
-//     for (int i = 0; i < 10; i++) {
-//         pthread_join(reader_threads[i], NULL);
-//     }
+    //wait for all reader threads to complete
+    for (int i = 0; i < 10; i++) {
+        pthread_join(reader_threads[i], NULL);
+    }
 
-//     //wait for all writer threads to complete
-//     for (int i = 0; i < randomVal; i++) {
-//         pthread_join(writer_threads[i], NULL);
-//     }
+    //wait for all writer threads to complete
+    for (int i = 0; i < randomVal; i++) {
+        pthread_join(writer_threads[i], NULL);
+    }
 
-//     // destroy semaphores
-//     destroy_sem(&resource_sem3);
-//     destroy_sem(&mutex_sem);
-//     return 0;
-// }
+    // destroy semaphores
+    destroy_sem(&resource_sem3);
+    destroy_sem(&mutex_sem);
+    return 0;
+}
 
 //solution3
 
